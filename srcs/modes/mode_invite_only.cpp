4168ea88494a9524ac7209c1d931733aa733e32e
intrusions <xeloronkali@gmail.com>
2023-11-24 13:09:11 +0100

    mode_topic && mode_invite_only refactor

--- srcs/modes/mode_invite_only.cpp
+++ srcs/modes/mode_invite_only.cpp
@@ -6,7 +6,7 @@
 /*   By: xel <xel@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:50:23 by xel               #+#    #+#             */
-/*   Updated: 2023/11/23 15:11:53 by xel              ###   ########.fr       */
+/*   Updated: 2023/11/24 13:05:08 by xel              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -15,8 +15,9 @@
 
 void    Server::_mode_invite_only(Channel *channel, bool add_or_rm) {
     
-    if (add_or_rm == REMOVE_MODE)       channel->set_mflags(channel->get_mflags() & ~CHANNEL_MODE_INVITE_ONLY);
-    else if (add_or_rm == ADD_MODE)     channel->set_mflags(channel->get_mflags() | CHANNEL_MODE_INVITE_ONLY);
+    add_or_rm == REMOVE_MODE 
+        ? channel->set_mflags(channel->get_mflags() & ~CHANNEL_MODE_INVITE_ONLY) 
+        : channel->set_mflags(channel->get_mflags() | CHANNEL_MODE_INVITE_ONLY);
 
     channel->set_is_invite_only(add_or_rm);
 }
--- srcs/modes/mode_topic_manage.cpp
+++ srcs/modes/mode_topic_manage.cpp
@@ -6,7 +6,7 @@
 /*   By: xel <xel@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:50:23 by xel               #+#    #+#             */
-/*   Updated: 2023/11/23 15:06:23 by xel              ###   ########.fr       */
+/*   Updated: 2023/11/24 13:06:20 by xel              ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -15,6 +15,7 @@
 
 void    Server::_mode_topic_manage(Channel *channel, bool add_or_rm) {
     
-    if (add_or_rm == REMOVE_MODE)       channel->set_mflags(channel->get_mflags() & ~CHANNEL_MODE_TOPIC_MANAGE);
-    else if (add_or_rm == ADD_MODE)     channel->set_mflags(channel->get_mflags() | CHANNEL_MODE_TOPIC_MANAGE);
+    add_or_rm == REMOVE_MODE
+        ? channel->set_mflags(channel->get_mflags() & ~CHANNEL_MODE_TOPIC_MANAGE)
+        : channel->set_mflags(channel->get_mflags() | CHANNEL_MODE_TOPIC_MANAGE);
 }
